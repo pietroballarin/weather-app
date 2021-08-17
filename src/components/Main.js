d8d0b49e5b7e83f9930889357fbc229298331947
Pietro Ballarin <pietroballarin@outlook.com>
2021-08-17 23:09:41 +0200

    adding temperature info and icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,19 @@ import React, {useState, useEffect} from 'react'
 import Searchbar from './Searchbar';
 import axios from 'axios';
 
-
-
 export default function Main() {
 
-    const [location, setLocation] = useState('');
-    const [weatherData, setWeatherData] = useState({weather: '', description: '', temperature: ''})
+    const weatherObj = {
+        weather: '', 
+        description: '', 
+        temperature: '',
+        tempMax: '',
+        tempMin: ''
+    };
+
+    const [location, setLocation] = useState('Sofia');
+    const [weatherData, setWeatherData] = useState(weatherObj);
+    const [weatherIcon, setWeatherIcon] = useState('')
 
     const handleLocationChange = (event) => {
         setLocation(event)
@@ -19,13 +26,18 @@ export default function Main() {
             setWeatherData({
                 weather: response.data.weather[0].main,
                 description: response.data.weather[0].description,
-                temperature: response.data.main.temp
+                temperature: (response.data.main.temp-273.15).toFixed(1),
+                tempMax: (response.data.main.temp_max-273.15).toFixed(1),
+                tempMin: (response.data.main.temp_min-273.15).toFixed(1)
             })
+            setWeatherIcon(response.data.weather[0].icon)
             console.log(response.data);
         }).catch(function (error) {
             console.error(error);
         });
-    });
+    }, []);
+
+    const imageLink = `http://openweathermap.org/img/wn/${weatherIcon}.png`
 
     return (
         <div>
@@ -33,9 +45,12 @@ export default function Main() {
             location={location}
             handleLocationChange={handleLocationChange}
             />
+            <img src={imageLink} alt="cloudy"></img>
             <h1>{weatherData.weather}</h1>
             <p>{weatherData.description}</p>
-            <h2>{weatherData.temperature}</h2>
+            <h2>{weatherData.temperature}°C</h2>
+            <p>Max {weatherData.tempMax}°C</p>
+            <p>Min {weatherData.tempMin}°C</p>
         </div>
     )
 }
