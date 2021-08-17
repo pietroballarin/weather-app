349e2cfa1c347cd34826a49b5a3cc89042ad9770
Pietro Ballarin <pietroballarin@outlook.com>
2021-08-17 16:49:20 +0200

    weather api responding

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Searchbar from './Searchbar';
+import axios from 'axios';
 
 export default function Main() {
 
@@ -8,6 +9,16 @@ export default function Main() {
     const handleLocationChange = (event) => {
         setLocation(event)
     }
+
+
+    useEffect(() => {
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=32f3d81b930a554de1f8f5b3e81b903f`)
+        .then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    })
 
     return (
         <div>
