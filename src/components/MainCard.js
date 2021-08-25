07451ef322d507b740eacaf57c935ccaf6608543
Pietro Ballarin <pietroballarin@outlook.com>
2021-08-25 10:44:28 +0200

    weekly weather component almost done

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -22,7 +22,7 @@ export default function MainCard(props) {
 
     useEffect(() => {
         
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${props.location}&appid=`)
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${props.location}&appid=32f3d81b930a554de1f8f5b3e81b903f`)
         .then(function (response) {
             // console.log(response.data);
             setWeatherData({
--- src/components/WeekCards.js
+++ src/components/WeekCards.js
@@ -5,14 +5,18 @@ export default function WeekCards(props) {
 
     const [weeklyData, setWeeklyData] = useState([])
 
+    console.log(weeklyData.map(day=>day.temp.day))
+
     const coor = props.savedCoordinates
+
+    const weekDaysArr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
     useEffect(() => {
         
-        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coor.lat}&lon=${coor.lon}&exclude=current,minutely,hourly&appid=`)
+        axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coor.lat}&lon=${coor.lon}&exclude=current,minutely,hourly&appid=32f3d81b930a554de1f8f5b3e81b903f`)
         .then(function (response) {
             console.log(response.data.daily);
-            setWeeklyData({weather: response.data})
+            setWeeklyData(response.data.daily)
         }).catch(function (error) {
             console.error(error);
         });
@@ -20,10 +24,27 @@ export default function WeekCards(props) {
 
     return (
         <div>
-            <h2></h2>
-            <img></img>
-            <p></p>
-            <p></p>
+            {weekDaysArr.map(function(day, index){
+                return (
+                    <div key={index}>
+                        <h2>{day}</h2>
+                    </div>
+                )
+            })}
+
+            {weeklyData.map(function (day, index){
+                return (
+                    <div key={index}>
+                        <img 
+                        src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                        alt={day.weather[0].main}
+                        >
+                        </img>
+                        <h3>{day.weather[0].main}</h3>
+                        <p>{(day.temp.day - 273.15).toFixed(1)}°C</p>
+                    </div>
+                )   
+            })}
         </div>
     )
 }
